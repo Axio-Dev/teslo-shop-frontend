471745bdac73d05074532ce5fb4ddfccc63db327
Axio Dev <axelvazmar0@gmail.com>
2026-07-12 13:21:50 -0600

    feat(Components/General Design): divide y añade la aplicación en componentes para sentar las bases del diseño en general

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,13 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron';
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Hola mundo</h1>
-      <h1 className="font-montserrat font-thin">Hola mundo</h1>
-      <h1 className="font-montserrat font-normal">Hola mundo</h1>
-      <h1 className="font-montserrat font-bold">Hola mundo</h1>
-      <Button>Hola mundo</Button>
-    </div>
+    <>
+      <CustomJumbotron title="Todos los productos" />
+    </>
   );
 };
