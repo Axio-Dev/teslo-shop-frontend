80dfced262c3dbbb49e689aa184adc4f19ce0652
Axio Dev <axelvazmar0@gmail.com>
2026-07-18 18:56:42 -0600

    feat(Project): añade zustand para manejar el estado global de la app

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,3 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { useCounterStore } from '@/auth/store/auth.store';
+
 export const ProductPage = () => {
-  return <div>ProductPage</div>;
+  const { inc, dec, count, incBy } = useCounterStore();
+
+  return (
+    <>
+      <h1 className="text-3xl font-montserrat">Count: {count} </h1>
+      <Button onClick={inc}>+1</Button>
+      <Button onClick={dec}> -1</Button>
+      <Button onClick={() => incBy(5)}> +5</Button>
+    </>
+  );
 };
