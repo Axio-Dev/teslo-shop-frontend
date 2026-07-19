d4f42459ef81f53b2c4d23875b6d798ccba04a11
Axio Dev <axelvazmar0@gmail.com>
2026-07-18 23:36:20 -0600

    feat(src): añade un CheckAuthProvider
    Este provider se agrega para que la app cheque de manera automática el token de usuario, este chequeo se llevara cada hora y media. Además si agrega una pantalla de carga con un spinner

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,17 +1,43 @@
 import { RouterProvider } from 'react-router';
 import { appRouter } from './app.router';
+import { type PropsWithChildren } from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
+import { checkAuthAction } from './auth/actions/check-auth.action';
+import { CustomFullScreenLoading } from './components/custom/CustomFullScreenLoading';
 
 const queryClient = new QueryClient();
+
+const CheckAuthProvider = ({ children }: PropsWithChildren) => {
+  const { isLoading } = useQuery({
+    queryKey: ['auth'],
+    queryFn: checkAuthAction,
+    retry: false,
+    refetchInterval: 1000 * 60 * 60 * 1.5, // revalidando cada hora y media si el token está activo
+    refetchOnWindowFocus: true,
+  });
+
+  if (isLoading) return <CustomFullScreenLoading />;
+
+  return children;
+};
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <RouterProvider router={appRouter} />
+
+      {/* Custom Provider */}
+      <CheckAuthProvider>
+        <RouterProvider router={appRouter} />
+      </CheckAuthProvider>
+
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
