230996b57c532c512163d4343b08f3bf4196e2b0
Axio Dev <axelvazmar0@gmail.com>
2026-07-18 22:54:31 -0600

    feat(auth): añade la acción para el endpoint de check-status y un interceptor en tesloApi
    El intercepetor se usa para:
    1. Toda request que pase por tesloApi tratará de ponerle el token, si el endpoint no lo necesita lo ignorará
    2. Evitar duplicar código para cada request, ahora será global
    3. Como TanStack está muy bien optimizado no importa que cada request pase por ahí
    
    Nuestra acción hace lo siguiente:
    1. Hace la petición a check-status para saber si nuestro usuario está autenticado
    2. Si no está autenticado lanza un error 'Token not foun'
    3. Si está le asigna el token encontrado
    4. Si el token existe pero no es válido, le retorna un error 'Token expired or not valid'

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -4,6 +4,15 @@ const tesloApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// TODO: Interceptores
+// Interceptores
+tesloApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem('tokne');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export { tesloApi };
